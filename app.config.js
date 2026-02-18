3fba9d589bbec5d1713624285727e4742579ab55
Dhrubo Talukder <dhrubo.dh@gmail.com>
2026-02-18 23:58:02 +0600

    fixed goole play console audio plugin problem

--- app.config.js
+++ app.config.js
@@ -50,6 +50,9 @@ export default {
         "MODIFY_AUDIO_SETTINGS",
         "android.permission.PACKAGE_USAGE_STATS",
       ],
+      blockedPermissions: [
+        "android.permission.RECORD_AUDIO",
+      ],
       config: {
         googleMobileAds: {
           app_id: "ca-app-pub-5474984690525462~6816196886",
